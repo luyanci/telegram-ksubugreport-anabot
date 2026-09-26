37e1c3aa821fb9e59b98868ffa02cd5c5c93c9e2
YC酱luyancib <luyancib@qq.com>
2026-09-26 15:35:59 +0800

    Update analog.py

--- analog.py
+++ analog.py
@@ -17,9 +17,9 @@ def unpack_tar_gz(file_path, output_path):
     
     with tarfile.open(file_path, 'r:gz') as tar:
             for file in tar.getmembers():
-                if file.size > MAX_FILE_SIZE:
-                    logger.warning(f"Skipping extraction of {file.name} due to size > {MAX_FILE_SIZE} bytes.")
-                    continue
+                # if file.size > MAX_FILE_SIZE:
+                #     logger.warning(f"Skipping extraction of {file.name} due to size > {MAX_FILE_SIZE} bytes.")
+                #     continue
                 try:
                     tar.extract(file, output_path)
                 except Exception as e:
